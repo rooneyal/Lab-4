243c35e53793333bb0c3343bd09f0ba7156106f9
rooneyal <rooneyal@bc.edu>
2020-10-13 16:29:40 -0400

    c3

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-d3.csv('wealth-health-2014.csv', d=>{
-    return {
-        ...d,
-    Income: +d.Income,
-    Population: +d.Population,
-    LifeExpectancy: +d.LifeExpectancy
-    }
-}).then(data=>{
-    Data = data
-    console.log('wealth', data);
-})
+d3.csv('wealth-health-2014.csv', d3.autoType).then(data=>{
+    console.log('wealth-health-2014.csv', data)
+
+    data = data.sort((a,b)=>b.income-a.income);
+
 
 const margin = {top:20, left:20, bottom:20, right:20};
 const width = 400-margin.left- margin.right;
@@ -20,30 +14,39 @@ const svg = d3.select('.chart').append('svg')
     .append('g')
     .attr('transform', 'translate('+margin.left+','+margin.right+')')
 
-
 const xScale = d3.scaleLinear()
     .domain([d3.extent(data, d=>d.Income)])
     .range([0, width]);
 
-const colorScale = d3.scaleSequential(d3.interpolateBlues)
-    .domain(d3.extent(data, d=>d.Income));
+const incomeMin = d3.min(data, d=>d.Income), 
+      incomeMax = d3.max(data, d=>d.Income);
 
+const incomeScale = d3.scaleLinear()
+    .domain([incomeMin, incomeMax])
+    .range([0, width]);
 
 const xAxis = d3.axisBottom()
     .scale(incomeScale)
     .ticks(5, 's');
+ 
+const colorScale = d3.scaleSequential(d3.interpolateBlues)
+    .domain([incomeMin, incomeMax]);
 
 svg.append('g')
-    .call(xAxis)
+    .attr("class", "axis x-axis")
+    .call(xAxis);
 
 
 svg.selectAll('.income')
     .data(data)
     .enter()
     .append('circle')
-    attr('fill', d=>colorScale(d.Income))
+    .attr('fill', d=>colorScale(d.Income))
     .attr('class', 'income')
     .attr('stroke', 'black')
-        .attr('opacity', 0.8)
     .attr('r', 5)
-    .attr('cx', d=>xScale(d.Income));
+    .attr('cy', height/2)
+    .attr('cx', d=>incomeScale(d.Income));
+
+
+})
