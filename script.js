8dc67efcfe1981c0776718754f446510636db8ef
rooneyal <rooneyal@bc.edu>
2020-10-13 21:35:13 -0400

    c4

--- script.js
+++ script.js
@@ -3,20 +3,15 @@ d3.csv('wealth-health-2014.csv', d3.autoType).then(data=>{
 
     data = data.sort((a,b)=>b.income-a.income);
 
-
-const margin = {top:20, left:20, bottom:20, right:20};
-const width = 400-margin.left- margin.right;
-const height = 300-margin.top- margin.bottom;
+const margin = {top:75, left:75, bottom:75, right:75};
+const width = 700-margin.left- margin.right;
+const height = 800-margin.top- margin.bottom;
 
 const svg = d3.select('.chart').append('svg')
     .attr('width', width +margin.left +margin.right)
     .attr('height', height +margin.top +margin.bottom)
     .append('g')
-    .attr('transform', 'translate('+margin.left+','+margin.right+')')
-
-const xScale = d3.scaleLinear()
-    .domain([d3.extent(data, d=>d.Income)])
-    .range([0, width]);
+    .attr('transform', 'translate('+margin.left+','+margin.top+')')
 
 const incomeMin = d3.min(data, d=>d.Income), 
       incomeMax = d3.max(data, d=>d.Income);
@@ -25,27 +20,51 @@ const incomeScale = d3.scaleLinear()
     .domain([incomeMin, incomeMax])
     .range([0, width]);
 
+const LifeExpectancyMin = d3.min(data, d=>d.LifeExpectancy), 
+      LifeExpectancyMax = d3.max(data, d=>d.LifeExpectancy);
+
+const LifeExpectancyScale = d3.scaleLinear()
+  .domain([LifeExpectancyMin, LifeExpectancyMax])
+  .range([0, width]);
+
+const PopulationMin = d3.min(data, d=>d.Population), 
+      PopulationMax = d3.max(data, d=>d.Population);
+
+const PopulationScale = d3.scaleLinear()
+.domain([PopulationMin, PopulationMax])
+.range([0, width]);
+
+
 const xAxis = d3.axisBottom()
     .scale(incomeScale)
     .ticks(5, 's');
  
+svg.append('g')
+    .attr("class", "axis x-axis")
+    .attr("transform", `translate(0, ${height})`)
+    .call(xAxis);
+    
+const yAxis = d3.axisLeft()
+    .scale(LifeExpectancyScale)
+    .ticks(5, 's');
+
+svg.append('g')
+    .attr("class", "axis y-axis")
+    .attr("transform", '0, translate(${width})')
+    .call(yAxis);
+
+
 const colorScale = d3.scaleSequential(d3.interpolateBlues)
     .domain([incomeMin, incomeMax]);
 
-svg.append('g')
-    .attr("class", "axis x-axis")
-    .call(xAxis);
-
-
-svg.selectAll('.income')
+svg.selectAll('.chart')
     .data(data)
     .enter()
     .append('circle')
     .attr('fill', d=>colorScale(d.Income))
-    .attr('class', 'income')
     .attr('stroke', 'black')
     .attr('r', 5)
-    .attr('cy', height/2)
+    .attr('cy', d=>LifeExpectancyScale(d.LifeExpectancy))
     .attr('cx', d=>incomeScale(d.Income));
 
 
